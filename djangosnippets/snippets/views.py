d8662147779c8782d50038415ed6bf52d0baaa9f
t-kotsubo <t_kotsubo@yahoo.co.jp>
2021-08-29 12:56:35 +0900

    1.2.5 まとめ 迄完了分をアップロード

--- djangosnippets/snippets/views.py
+++ djangosnippets/snippets/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def top(request):
+    return HttpResponse(b"Hello World")
+
+def snippet_new(request):
+    return HttpResponse('スニペットの登録')
+
+def snippet_edit(request, snippet_id):
+    return HttpResponse('スニペットの編集')
+
+def snippet_detail(request, snippet_id):
+    return HttpResponse('スニペットの詳細閲覧')
+
